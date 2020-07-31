18c52800172d2ffd954d33590cc04d6653885110
Jidaewook <psatdoctor@naver.com>
2020-07-31 13:36:12 +0900

    env setting

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
@@ -26,6 +28,6 @@ app.use(async function (err, req, res, next) {
     next();
 });
 
-const PORT = 7000;
+const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
