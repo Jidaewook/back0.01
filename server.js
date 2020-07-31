ffe5dc9920ae0fce63dfe6dced657bde17d52d52
Jidaewook <psatdoctor@naver.com>
2020-07-31 13:31:26 +0900

    initial setting

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use('./uploads/', express.static('uploades'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, OPTIONS");
+    next();
+});
+
+app.use(async function (err, req, res, next) {
+    console.error(err.message);
+    await res.status(500).json({
+        error: err.message
+    });
+    next();
+});
+
+const PORT = 7000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
